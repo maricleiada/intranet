157956ff73a84e5155c8817d8a73686dd2a4dd53
Maricleia da Silva Nascimento <132806138+maricleiada@users.noreply.github.com>
2025-08-25 07:10:23 -0300

    Delete backend directory

--- backend/prisma/migrations/20250818103323_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Site" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "url" TEXT NOT NULL,
-    "status" TEXT NOT NULL,
-    "lastChecked" DATETIME NOT NULL
-);
